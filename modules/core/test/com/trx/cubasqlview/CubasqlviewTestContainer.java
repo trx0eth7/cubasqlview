44d92a8e10aae0d3bf06ed2cdb638f1e2b033a47
Vasilev Alexander <vasilev@haulmont.com>
2021-06-06 19:05:25 +0400

    init commit

---
+++ modules/core/test/com/trx/cubasqlview/CubasqlviewTestContainer.java
@@ -0,0 +1,58 @@
+package com.trx.cubasqlview;
+
+import com.haulmont.cuba.testsupport.TestContainer;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class CubasqlviewTestContainer extends TestContainer {
+
+    public CubasqlviewTestContainer() {
+        super();
+        //noinspection ArraysAsListWithZeroOrOneArgument
+        appComponents = new ArrayList<>(Arrays.asList(
+                // list add-ons here: "com.haulmont.reports", "com.haulmont.addon.bproc", etc.
+                "com.haulmont.cuba"
+        ));
+        appPropertiesFiles = Arrays.asList(
+                // List the files defined in your web.xml
+                // in appPropertiesConfig context parameter of the core module
+                "com/trx/cubasqlview/app.properties",
+                // Add this file which is located in CUBA and defines some properties
+                // specifically for test environment. You can replace it with your own
+                // or add another one in the end.
+                "com/trx/cubasqlview/test-app.properties");
+        autoConfigureDataSource();
+    }
+
+    
+
+    public static class Common extends CubasqlviewTestContainer {
+
+        public static final CubasqlviewTestContainer.Common INSTANCE = new CubasqlviewTestContainer.Common();
+
+        private static volatile boolean initialized;
+
+        private Common() {
+        }
+
+        @Override
+        public void beforeAll(ExtensionContext extensionContext) throws Exception {
+            if (!initialized) {
+                super.beforeAll(extensionContext);
+                initialized = true;
+            }
+            setupContext();
+        }
+        
+
+        @SuppressWarnings("RedundantThrows")
+        @Override
+        public void afterAll(ExtensionContext extensionContext) throws Exception {
+            cleanupContext();
+            // never stops - do not call super
+        }
+        
+    }
+}
